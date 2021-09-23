a9205b9fc9d35ae51cb0c7545377625c83887b2c
Ca7Ac1 <Ca7Ac1@gmail.com>
2021-09-23 00:02:35 -0400

    Hw #3 solution

---
+++ Euler.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int multiple35()
+{
+    int sum = 0;
+
+    int i;
+    for (i = 0; i < 1000; i++)
+    {
+        if (i % 5 == 0 || i % 3 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+int fibEven()
+{
+    int sum = 0;
+
+    int a = 0;
+    int b = 1;
+    int c = 0;
+
+    while(a + b < 4000000)
+    {
+        c = a + b;
+
+        if (c % 2 == 0)
+        {
+            sum += c;
+        }
+
+        a = b;
+        b = c;
+    }
+
+    return sum;
+}
+
+unsigned long long largestPrimeFactor()
+{
+    unsigned long long num = 600851475143;
+    unsigned long long sol = 1;
+
+    unsigned long long i; 
+    for (i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            sol = i;
+        }
+
+        while (i <= num && num % i == 0)
+        {
+            num /= i;
+        }
+    }
+
+    return max(sol, num);
+}
+
+int smallestMultiple()
+{
+    int total = 1;
+
+    int i;
+    int j;
+
+    for (i = 1; i <= 20; i++)
+    {
+        int num = i;
+        int additional = 1;
+        for (j = i - 1; j > 1; j--)
+        {
+            while (num % j == 0 && total % j == 0)
+            {
+                total /= j;
+                num /= j;
+                additional *= j;
+            }
+        }
+
+        total *= num * additional;
+    }
+
+    return total;
+}
+
+int sumSquareDiff()
+{
+    int sum = 0;
+    int square = 0;
+
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        sum += i * i;
+        square += i;
+    }
+
+    square *= square;
+
+    return square - sum;
+}
+
+int main()
+{
+    printf("Solution to Multiples of 3 or 5: %d\n", multiple35());
+    printf("Solution to Even Fibonacci numbers: %d\n", fibEven());
+    printf("Solution to Largest prime factor: %llu\n", largestPrimeFactor());
+    printf("Solution to Smallest multiple: %d\n", smallestMultiple());
+    printf("Solution to Sum square difference: %d\n", sumSquareDiff());
+    return 0;
+}
